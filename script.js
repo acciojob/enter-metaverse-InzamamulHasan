334d5cfdc73454a5d561fc47cb3229cfacc37933
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-05-01 23:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let enterBtn=document.getElementById("enterBtn");
+let status=document.getElementById("status");
+enterBtn.addEventListener("click",()=>{
+	let h1=document.createElement("h1");
+	h1.innerText="Enter the Metaverse";
+	status.insertAdjacentElement("afterend",h1);
+	status.remove()
+})
